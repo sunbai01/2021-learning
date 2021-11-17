71f7ec85288e17ba50ebf4f47282667cbcf0d60b
sunbai01 <15732168026@163.com>
2021-11-17 20:36:59 +0800

    quardra-kill-toutiao-vue-语法-watch

--- penta-kill/vue/src/pages/items/agriculture.vue
+++ penta-kill/vue/src/pages/items/agriculture.vue
@@ -6,6 +6,7 @@
         <label>输入地区：</label>
         <input type="text" v-on:input="onInput"/>
         <span>地区为: {{area}}</span>
+        <button v-on:click="changePosition"></button>
         <span>猪价为: {{price}}</span>
     </div>
 </template>
@@ -21,11 +22,22 @@ export default {
     },
     create() {
         // 既然地区和价格是联动的，不用在每个地方都用queryPigPrice，业务中不要这么做，做到只与数据强相关
+        // 是下面watch关键字的api形式，api的形式可以动态watch
         this.$watch('area', area => {
             this.queryPigPrice(area)
         });
     },
+    // 监听一个属性
+    watch: {
+        area(newArea, area) {
+            this.queryPigPrice(newArea);
+        }
+    },
     methods: {
+        // 思想记一下，语法背一下就行
+        changePosition() {
+            this.area = "深圳";
+        },
         onInput(e) {
             this.area = e.data;
         },
