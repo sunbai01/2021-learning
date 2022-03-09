8e9bfc992f67b6d7eb05362860f358e96168aff4
sunbai01 <15732168026@163.com>
2022-03-10 00:30:34 +0800

    闭包，作用域，执行上下文

--- first-blood/simple.js
+++ first-blood/simple.js
@@ -1,10 +1,11 @@
 // 以下 note 一句废话也不会有
-
+(下面主要讲了三个知识点：闭包，作用域，执行上下文)
+// 一个函数
 function executionFuncOuter(){
     function executionFuncInner(){
-        // executionContext包含
+        // executionContext(执行上下文)包含
         // scopeChain
-        // variableObject
+        // variableObject（涉及变量提升 hoisting）
         // this
     }
 }
@@ -12,43 +13,70 @@ executionFuncOuter();
 
 # scopeChain（作用域链）
 
-如上函数会形成一个栈 // ECMA第六版的规范
+如上函数会形成一个执行栈 // ECMA第六版的规范，会在定义栈向上找，但不会在执行栈向上找
 |  inner() |
 |  outer() |
 |  window  |
 ------------
 
-// 作用域嵌套的时候，会在嵌套中寻找（也就是有关系的作用域内的变量可以相互访问），但不会找到同级的作用域（es6增加了块级作用域），这种链式关系叫做作用域链
-// QA1:？？？这句话后面看又似懂非懂了，要再回顾一下？？？
+// 会在定义栈向上找变量，但不会在执行栈向上找：这句话很重要
 
 # variableObject
 
-// 浏览器内部（内部机制，浏览器里看不到）会先声明变量，后面再去赋值，这种现象叫变量提升(hosting)
-// 直接console会define报错，需要 var xx;才会输出undefined
+// 浏览器内部（内部机制，浏览器里看不到）会先声明变量，后面再去赋值，这种现象叫变量提升(hoisting)
+// 直接console一个a会define报错，需要在 var a = 1;前console一个a，才会输出undefined
+
+# 作用域
+
+// js中有全局作用域，函数作用域，es6又增加了块级作用域
 
 # 闭包
 
 // 一个作用域访另一个作用域中的变量, 不是说不 return 就不是闭包了
+e.g
 
-// 一般做私有，怎么做私有呢？先举一个不私有的例子
+function outer() {
+    var person = 'sunbai';
+    function inner() {
+        console.log('person', person)
+    }
+    inner();
+}
+outer();
 
+// 改造 => 
+
+function outer() {
+    var person = 'sunbai';
+    return function inner() {
+        console.log('person', person)
+    }
+}
+var inner = outer();
+inner();
+
+## 闭包的应用场景：
+// 1、一般封装私有变量（AMD的框架都常用），怎么做私有呢？先举一个不私有的例子
+// 普通定义类的方式
 function Person() {
+    // 这个属性挂在了对象上
     this._attackValue = 100;
 }
 
 Person.prototype = {
-    attack: function(body) {
+    attack: function (body) {
         body.blood = this._attackValue - body.defense;
     }
 }  
 
 var person = new Person();
-console.log('person', person._attackValue);
+console.log('person', person._attackValue); // 100
 
-// 下面改造为私有：（工厂方法）
-
+// 下面改造为私有：（工厂方法重新来做这个事情）
+// 这是定义了一个类
 function Person() {
     var _attackValue = 100;
+    // 返回一个新对象
     return {
         attack: function(body) {
             body.blood = this._attackValue - body.defense;
@@ -57,9 +85,9 @@ function Person() {
 }
 
 var person = new Person();
-console.log('person', person._attackValue);
+console.log('person', person._attackValue); // undefined
 
-// AMD框架定义一个模块，这块没弄明白，到底是_attackValue共享吗，待验证，（同QA2:？？？这样_attackValue可以共用吗？？？）
+// AMD框架定义一个模块
 define(function () {
     var _attackValue = 100;
     // 这不算私有变量，这个叫做静态私有变量
@@ -77,13 +105,13 @@ define(function () {
 
 var Person = require('Person');
 
-
+// 不共享
 var person1 = new Person();
 var person2 = new Person();
 var person3 = new Person();
 
 
-// 以上变形存储变量,只攻击一次
+// 2、存储变量（只攻击一次）
 define(function () {
     var flag = false;
     // 定义一个类
@@ -105,7 +133,7 @@ button.addEventListener('click', function() {
     person.attack();
 })
 
-// 缓存策略，ajax写的也长，需要fetch一下(?这里需要一句话解释下ajax和fetch的区别)
+// 3、缓存策略，ajax写的也长，需要fetch一下(?这里需要一句话解释下ajax和fetch的区别)
 
 function getListDataManager() {
     let localData = null;
