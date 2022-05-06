b502f0341213911cce3b8cfb0b6fce2536f72c3a
sunbai01 <15732168026@163.com>
2022-05-06 09:10:16 +0800

    bfs - 迷宫

--- suanfa/bfs.js
+++ suanfa/bfs.js
@@ -1,93 +1,77 @@
-// dfs 广度优先遍历
+// bfs （broad） 广度优先遍历
 
-            1
+// 题干：有一个迷宫，有很多1障碍，0是可走的路，王子是（2），公主是（3），求王子救出公主最少需要多少步，
+// 并将路线输出出来
 
-  2         3       4
-
-5   6
-
-// 遍历结果可能有如下两种
-125634
-
-123456
-
-// 以上可以具象为
-
-                      document 
-
-    div                  h1              h2
-  
-  span  a[id=1]         a[id=2]
-
-
-// 做的事情是遍历一棵树下想要的某一元素节点，用bfs怎么做
-
-// 八皇后问题
 
 // 思路：
-// 1、函数体的构造
-// 2、判断x，y，斜线方向有没有冲突
-    // 2.1 x方向
-    // 2.2 y方向
-    // 2.3 斜线方向
-// 3、这里有一个极限值是说看超不超过8，超过return
-// 4、判断情况的话，需要将之前符合情况的棋子再次置为0，然后放置棋子
+// 1、影分身，每到达一个点，上下左右都试一下（涟漪走开），看有没有碰到公主，碰到就赢了
+// 而不是像dfs一样一路走到黑，再退回来。
+// 2、
+// 3、
+const getMaze = () => {
+    return [
+        [2, 0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 1, 0],
+        [0, 1, 1, 0, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 3, 0]
+    ];
+};
 
-const getChessboard = (size = 8) => {
-    let chessBoard = [];
-    for (let i = 0; i < size; i++) {
-        chessBoard.push(Array(8).fill(0));
-    }
-
-    return chessBoard;
+const createPoint = ({x, y, stepCount}) => {
+    return {x, y, stepCount};
 }
 
-const judge = (chessBoard, x, y) => {
-    const len = chessBoard.length;
-    for (let i = 0; i< len; i++) {
-        const rowHasClash = chessBoard[x][i] === 1;
-        const colHasClash = chessBoard[i][y] === 1;
-        const directions = [
-            [1, 1],
-            [1, -1],
-            [-1, 1],
-            [-1, -1]
-        ]
-        // 四向斜线有没有冲突
-        const slashHasClash = directions.some(([xMark, yMark]) => {
-            const newX = x + xMark * (i + 1);
-            const newY = y + yMark * (i + 1);
-            return chessBoard[newX] && chessBoard[newX][newY] === 1;
-        })
-        if ( rowHasClash || colHasClash || slashHasClash) {
-            return false;
+const judge = (maze, {x, y}) => {
+    return (x>=0)&& (y >=0) 
+        && (x< maze.length) && (y< maze[0].length)
+        && (maze[x][y] !== 1);
+}
+
+const run = (maze, maru) => {
+
+    const maruPoint = maru;
+    let stepsQueue = [maruPoint];
+
+    const steps = [
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0, -1]
+    ];
+
+    while (stepsQueue.length) {
+        const curPoint = stepsQueue.shift();
+        for (let i in steps )  {
+            const [vectorX, vectorY] = step[i];
+            const newX = curPoint.x + vectorX;
+            const newY = curPoint.y + vectorY;
+            if (judge(maze, {x: newX, y: newY})) {
+
+                const newPoint = createPoint({ 
+                    x: newX,
+                    y: newY,
+                    stepCount: curPoint.stepCount + 1,
+                    prevPoint: curPoint
+                });
+
+                if (maze[newPoint.x][newPoint.y] == 3) {
+                    console.log('找到公主啦')
+                }
+                maze[newPoint.x, newPoint.y] = 1;
+                stepsQueue.push(newPoint);
+            }
         }
     }
-    return true;
-}
-
-let count = 0;
-const drop = (chessBoard, depth, maxDepth) => {
-    if(depth > maxDepth) {
-        count++;
-        return;
-    }
-    for (let i = 0; i < maxDepth; i++ ) {
-        if(judge(chessBoard,depth, i)) {
-            chessBoard[depth][i] = 1;
-            drop(chessBoard, depth + 1, maxDepth);
-            chessBoard[depth][i] = 0;
-        }
-    }
-}
+};
 
 const start = () => {
-    const chessboard = getChessboard(3);
-    drop(chessboard, 0, chessboard.length);
-    console.log('count：：：', count);
-}
+   let maze = getMaze();
+   run(maze, { x : 0, y: 0});
+};
 
 start();
-
-
 
